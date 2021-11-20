51d4bca6e7d62c06daed7ba517f7a784ce598a65
Thelostsinofenvy <33452588+Thelostsinofenvy@users.noreply.github.com>
2021-11-20 21:15:44 +0530

    some changes

--- notes/admin.py
+++ notes/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-
-from notes.models import Notes
+from notes.models import Note
 from notes import models
 
 # Register your models here.
-admin.site.register(Notes)
+admin.site.register(Note)
--- notes/models.py
+++ notes/models.py
@@ -1,8 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
 
-class Notes(models.Model):
+class Note(models.Model):
     name = models.CharField(max_length=60, editable=True)
     text = models.TextField(max_length=300, null=True, blank=True)
+    date_created = models.DateTimeField(
+        default=timezone.datetime.now())
+
+    def __str__(self):
+        return self.name
