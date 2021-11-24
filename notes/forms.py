ca20f27fa4c2f909fd9a9c3f0c5d2a00656deb92
Thelostsinofenvy <33452588+Thelostsinofenvy@users.noreply.github.com>
2021-11-24 22:09:36 +0530

    added model forms

---
+++ notes/forms.py
@@ -0,0 +1,14 @@
+from .models import Note
+from django import forms
+from django.forms import ModelForm, fields, widgets
+
+
+class NoteForm(forms.ModelForm):
+    heading = forms.CharField(max_length=100, widget=forms.TextInput(attrs={
+                              'placeholder': 'Enter Title'}))
+    text = forms.CharField(max_length=500, widget=forms.TextInput(attrs={
+        'placeholder': 'Write your notes here!'}))
+
+    class Meta:
+        model = Note
+        fields = ['heading', 'text']
