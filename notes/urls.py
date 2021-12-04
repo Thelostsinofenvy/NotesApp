1fb50c6babd1852cda8b408eb21ab4ff9d9c9ae8
Thelostsinofenvy <33452588+Thelostsinofenvy@users.noreply.github.com>
2021-12-04 21:09:43 +0530

    added delete url

--- notes/urls.py
+++ notes/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('new', views.addNote, name='new'),
     path('details/<int:id>', views.details, name='details'),
-    path('edit/<int:id>', views.edit, name='edit')
+    path('edit/<int:id>', views.edit, name='edit'),
+    path('delete/<int:id>', views.delete, name='delete')
 ]
